2f484aad46d7ffcaea7a7a1e7cfc2ae36b96559c
koudaikumazaki <kk1974340@gmail.com>
2022-02-12 18:29:31 +0900

    fix: schema

--- database/migrations/2022_02_12_082717_create_likes_table.php
+++ database/migrations/2022_02_12_082717_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStoresTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateStoresTable extends Migration
      */
     public function up()
     {
-        Schema::create('stores', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->text('description');
-            $table->foreignId('user_id')->constrained('users');
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete("cascade");
+            $table->foreignId('store_id')->constrained('stores')->onDelete("cascade");
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateStoresTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('stores');
+        Schema::dropIfExists('likes');
     }
 }
